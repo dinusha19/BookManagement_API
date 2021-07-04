d9972c2d6425c012ed1ae86fb83f873567d90722
dinusha19 <sdinusha842@gmail.com>
2021-07-04 20:28:42 +0530

    added exception handling

--- database/book.js
+++ database/book.js
@@ -3,8 +3,18 @@ const mongoose = require("mongoose");
 //creating a book schema
 const BookSchema = mongoose.Schema({
     
-    ISBN:String,
-    title:String,
+    ISBN:{
+        type:String,
+        required:true,
+        minLength:8,
+        maxLength:10
+    },
+    title:{
+        type:String,
+        required:true,
+        minLength:8,
+        maxLength:10
+    },
     pubDate:String,
     language:String,
     numPage:Number,
